31af1fddaac6bc8151c7eea2a603cc7be45cd275
adarsh sharma <37371998+adarshsharma27@users.noreply.github.com>
2024-01-13 00:12:18 +0530

    feat: Mobile Navigation Integration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
-import React from "react";
-import { NavLink,Link } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
 import { RiLinksFill, RiAccountPinCircleLine } from "react-icons/ri";
+import { RiAlignLeft, RiCloseLine } from "react-icons/ri";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+  const toggleNavigation = () => {
+    setOpen(!open);
+  };
   return (
     <>
       <header className="bg-white  font-poppins">
-        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 hidden md:block">
           <div className="flex h-16 items-center justify-between">
             <div className="md:flex md:items-center md:gap-12">
               <Link className="flex font-bold text-indigo-600" to="/">
@@ -20,10 +25,13 @@ const Header = () => {
                 <ul className="flex items-center gap-6 text-base">
                   <li>
                     <NavLink
-                     className="flex gap-2 hover:bg-slate-200 hover:text-indigo-600 p-2 rounded"
+                      className="flex gap-2 hover:bg-slate-200 hover:text-indigo-600 p-2 rounded"
                       to="/"
                     >
-                    <RiLinksFill size={20} className="hover:text-indigo-600"/>
+                      <RiLinksFill
+                        size={20}
+                        className="hover:text-indigo-600"
+                      />
                       Links
                     </NavLink>
                   </li>
@@ -33,7 +41,7 @@ const Header = () => {
                       className="flex gap-2 hover:bg-slate-200 hover:text-indigo-600 p-2 rounde"
                       to="/profileDetails"
                     >
-                    <RiAccountPinCircleLine size={20} />
+                      <RiAccountPinCircleLine size={20} />
                       Profile Details
                     </NavLink>
                   </li>
@@ -59,26 +67,79 @@ const Header = () => {
                   </a>
                 </div>
               </div>
+            </div>
+          </div>
+        </div>
+        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 block md:hidden">
+          <div className="flex h-16 items-center justify-between">
+            <div className="md:flex md:items-center md:gap-12">
+              <Link className="flex font-bold text-indigo-600" to="/">
+                <span className="sr-only">Home</span>
+                <RiLinksFill size={30} /> <span>SimpleShare</span>
+              </Link>
+            </div>
 
-              <div className="block md:hidden">
-                <button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                </button>
+            <div className="flex items-center gap-4">
+              <div className="block md:hidden z-50">
+                {open === true ? (
+                  <RiCloseLine
+                    className="text-2xl"
+                    onClick={() => toggleNavigation()}
+                  />
+                ) : (
+                  <RiAlignLeft
+                    className="text-2xl"
+                    onClick={() => toggleNavigation()}
+                  />
+                )}
               </div>
             </div>
+          </div>
+          <div
+            className={
+              open === true
+                ? "md:hidden flex flex-col absolute right-0 top-0 w-full h-screen transition-all duration-1000 ease-in-out"
+                : "md:hidden flex flex-col absolute right-[-350px] top-0 w-full h-screen transition-all duration-1000 ease-in-out"
+            }
+          >
+            <nav aria-label="Global">
+              <div className="w-full h-screen bg-indigo-600 flex flex-col justify-center items-center gap-4">
+                <NavLink
+                  className="flex gap-2 hover:bg-slate-200 hover:text-indigo-600 p-2 rounded"
+                  to="/"
+                  onClick={() => toggleNavigation()}
+                >
+                  <RiLinksFill size={20} className="hover:text-indigo-600" />
+                  Links
+                </NavLink>
+
+                <NavLink
+                  className="flex gap-2 hover:bg-slate-200 hover:text-indigo-600 p-2 rounded"
+                  to="/profileDetails"
+                  onClick={() => toggleNavigation()}
+                >
+                  <RiAccountPinCircleLine size={20} />
+                  Profile Details
+                </NavLink>
+
+                <div className="p-2">
+                  <a
+                    className="rounded-md bg-indigo-600 px-5 py-2.5 text-sm font-medium text-white shadow"
+                    href="/"
+                  >
+                    Login
+                  </a>
+                </div>
+                <div className="p-2">
+                  <a
+                    className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-indigo-600"
+                    href="/"
+                  >
+                    Register
+                  </a>
+                </div>
+              </div>
+            </nav>
           </div>
         </div>
       </header>
