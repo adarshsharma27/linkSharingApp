d0ed7b416958272f1a51fbc805913f4d4f301ee7
adarsh sharma <37371998+adarshsharma27@users.noreply.github.com>
2024-02-08 23:36:42 +0530

    feat: Profile Image Upload functionality

--- src/components/ProfileDetails.jsx
+++ src/components/ProfileDetails.jsx
@@ -8,10 +8,11 @@ import {
   RiAccountPinCircleFill,
   RiAB,
   RiUploadCloud2Fill,
+  RiCloseFill,
 } from "react-icons/ri";
 import toast from "react-hot-toast";
 import conf from "../conf/config";
-import { ID, databases } from "../conf/config";
+import { ID, databases, storage } from "../conf/config";
 
 const ProfileDetails = () => {
   const dispatch = useDispatch();
@@ -19,6 +20,9 @@ const ProfileDetails = () => {
   const [firstName, setFirstName] = useState();
   const [lastName, setLastName] = useState();
   const [email, setEmail] = useState();
+  const [imageUrl, setImageUrl] = useState("");
+  const [uploadFileId, setUploadFileId] = useState("");
+  const [hideFileUpload, setHideFileUpload] = useState(true);
   const userProfileDetails = useSelector((state) => state?.profileReducer);
   const userProfileLinksDetails = useSelector((state) => state?.profileReducer);
   useEffect(() => {
@@ -31,6 +35,119 @@ const ProfileDetails = () => {
   const instaGramUrl = userProfileLinksDetails?.profileLinks?.instaGramUrl;
   const faceBookUrl = userProfileLinksDetails?.profileLinks?.faceBookUrl;
   const twitterUrl = userProfileLinksDetails?.profileLinks?.twitterUrl;
+  const handleImage = (e) => {
+    const image = e.target.files[0];
+    const imageType = image?.type.split("/")[1];
+    const imageSize = image?.size;
+    debugger;
+    if (
+      imageType != "jpeg" &&
+      imageType != "jpg" &&
+      imageType != "jpeg" &&
+      imageType != "gif"
+    ) {
+      toast.error("Please Upload PNG, JPG or GIF  Format", {
+        duration: 4000,
+        position: "top-right",
+        style: {
+          background: "#fff",
+          color: "#252525",
+          padding: "20px",
+          fontWeight: "700",
+          boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.1)",
+          borderBottom: "3px solid #F17171",
+          borderRadius: "3px",
+          fontFamily: "Poppins, sans-serif",
+        },
+      });
+    } else if (imageSize > 2000000) {
+      toast.error("Please Upload File Size less then 2MB", {
+        duration: 4000,
+        position: "top-right",
+        style: {
+          background: "#fff",
+          color: "#252525",
+          padding: "20px",
+          fontWeight: "700",
+          boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.1)",
+          borderBottom: "3px solid #F17171",
+          borderRadius: "3px",
+          fontFamily: "Poppins, sans-serif",
+        },
+      });
+    } else {
+      const promise = storage.createFile(conf.bucketId, ID.unique(), image);
+      promise.then(
+        function (response) {
+          const fileId = response.$id;
+          if (fileId) {
+            const imgUrl = storage.getFilePreview(conf.bucketId, fileId);
+            if (imgUrl?.href) {
+              setImageUrl(imgUrl?.href);
+              // setCreateDisable(false);
+              setUploadFileId(fileId);
+              setHideFileUpload(false);
+            }
+          }
+        },
+        function (error) {
+          toast.error(error.message, {
+            duration: 4000,
+            position: "top-right",
+            style: {
+              background: "#fff",
+              color: "#252525",
+              padding: "20px",
+              fontWeight: "700",
+              boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.1)",
+              borderBottom: "3px solid #F17171",
+              borderRadius: "3px",
+              fontFamily: "Poppins, sans-serif",
+            },
+          }); // Failure
+        }
+      );
+    }
+  };
+  const deleteImage = () => {
+    setImageUrl("");
+    const promise = storage.deleteFile(conf.bucketId, uploadFileId);
+    promise.then(
+      function () {
+        toast.success("Image Removed Successfully", {
+          duration: 4000,
+          position: "top-right",
+          style: {
+            background: "#fff",
+            color: "#252525",
+            padding: "20px",
+            fontWeight: "700",
+            boxShadow: "0px 2px 10px rgba(0, 0, 0, 0.1)",
+            borderBottom: "3px solid #4F46E5",
+            borderRadius: "3px",
+            fontFamily: "Poppins, sans-serif",
+          },
+        });
+        setHideFileUpload(true);
+      },
+      function (error) {
+        toast.error(error.message, {
+          duration: 4000,
+          position: "top-right",
+          style: {
+            background: "#fff",
+            color: "#252525",
+            padding: "20px",
+            fontWeight: "700",
+            boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.1)",
+            borderBottom: "3px solid #F17171",
+            borderRadius: "3px",
+            fontFamily: "Poppins, sans-serif",
+          },
+        }); // Failure
+      }
+    );
+  };
   const userLinksProfileDetails = async (e) => {
     dispatch(addUserDetails({ firstName, lastName, email }));
 
@@ -183,29 +300,51 @@ const ProfileDetails = () => {
                 >
                   Profile Picture
                 </label>
-                <div className="flex items-center justify-center w-full">
-                  <label
-                    for="dropzone-file"
-                    className="flex flex-col items-center justify-center w-full h-54 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 hover:border-indigo-600 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-                  >
-                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                      <RiUploadCloud2Fill
-                        size={30}
-                        className="text-indigo-600"
+
+                {hideFileUpload && (
+                  <div className="flex items-center justify-center w-full">
+                    <label
+                      for="dropzone-file"
+                      className="flex flex-col items-center justify-center w-full h-54 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 hover:border-indigo-600 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                    >
+                      <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                        <RiUploadCloud2Fill
+                          size={30}
+                          className="text-indigo-600"
+                        />
+                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                          <span className="font-semibold">Click to upload</span>{" "}
+                          <span className="text-indigo-600 font-bold lg:text-xl text-base">
+                            Profile Picture
+                          </span>
+                        </p>
+                        <p className="text-xs text-gray-500 dark:text-gray-400">
+                          SVG, PNG, JPG or GIF (MAX-2MB)
+                        </p>
+                      </div>
+                      <input
+                        id="dropzone-file"
+                        type="file"
+                        className="hidden"
+                        onChange={handleImage}
                       />
-                      <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                        <span className="font-semibold">Click to upload</span>{" "}
-                        <span className="text-indigo-600 font-bold lg:text-xl text-base">
-                          Profile Picture
-                        </span>
-                      </p>
-                      <p className="text-xs text-gray-500 dark:text-gray-400">
-                        SVG, PNG, JPG or GIF (MAX-2MB)
-                      </p>
+                    </label>
+                  </div>
+                )}
+                {imageUrl && (
+                  <>
+                    <div className="p-2 w-full">
+                      <div className="relative flex flex-col items-center justify-center p-2 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-100 bg-opacity-50 dark:hover:bg-bray-800 dark:bg-slate-700  dark:border-white">
+                        <img src={imageUrl} className="relative" />
+                        <RiCloseFill
+                          size={40}
+                          className=" bg-indigo-600 text-white p-1 hover:bg-red-400 transition hover:scale-110  hover:cursor-pointer dark:text-white absolute top-6 right-6 rounded-lg"
+                          onClick={() => deleteImage()}
+                        />
+                      </div>
                     </div>
-                    <input id="dropzone-file" type="file" className="hidden" />
-                  </label>
-                </div>
+                  </>
+                )}
               </div>
 
               <div className="flex items-center justify-between">
